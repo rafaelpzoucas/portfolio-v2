96ec5c6cbae86a189ed6aa3282280fc8f79e6b28
Rafael Zoucas <rafaelpzoucas@gmail.com>
2023-11-29 19:55:19 -0300

    Update contact.tsx

--- src/app/(sections)/contact.tsx
+++ src/app/(sections)/contact.tsx
@@ -1,16 +1,19 @@
 import { Button } from '@/components/ui/button'
 import { H1, H3 } from '@/components/ui/headings'
 import { Section } from '@/components/ui/section'
+import { ArrowRight } from 'lucide-react'
 
 export function Contact() {
   return (
     <Section id="contact" className="sm:h-screen sm:px-20">
       <header className="flex flex-col gap-3 w-full">
-        <H1>Vamos iniciar um projeto juntos?</H1>
+        <H1>Pronto para Transformar sua Visão em Realidade?</H1>
         <p className="text-muted-foreground">
-          Transformando ideias em projetos excepcionais
+          Tire suas ideias do papel. Estou aqui para transformá-las realidade.
         </p>
-        <Button className="w-full sm:w-fit mt-4">Iniciar projeto</Button>
+        <Button className="w-full sm:w-fit mt-4">
+          Iniciar projeto <ArrowRight className="w-4 h-4 ml-2" />
+        </Button>
       </header>
 
       <article className="w-full space-y-4">
