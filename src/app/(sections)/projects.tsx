9c52b92b1e0bff6f6fa48683ed56a0f361e10d1e
Rafael Zoucas <rafaelpzoucas@gmail.com>
2023-12-06 12:19:21 -0300

    Update projects.tsx

--- src/app/(sections)/projects.tsx
+++ src/app/(sections)/projects.tsx
@@ -4,6 +4,7 @@ import { H1, H3 } from '@/components/ui/headings'
 import { Section } from '@/components/ui/section'
 import { ArrowRight } from 'lucide-react'
 import Image from 'next/image'
+import Link from 'next/link'
 import { projects } from './data'
 
 export function Projects() {
@@ -14,41 +15,41 @@ export function Projects() {
 
       <section className="sm:px-0 space-y-16 w-full">
         {projects.slice(0, 3).map((project) => (
-          <article
-            key={project.title}
-            className="flex flex-col sm:flex-row gap-8 w-full"
-          >
-            <div className="relative sm:min-w-[290px] h-[195px] rounded-lg bg-secondary overflow-hidden">
-              <Image
-                src={project.image_src}
-                alt={project.title}
-                fill
-                className="object-cover"
-              />
-            </div>
-            <aside className="flex flex-col gap-4">
-              <H3>{project.title}</H3>
-              <p className="text-muted-foreground">{project.description}</p>
-              <footer className="flex flex-row gap-2">
-                {project.stacks.map((stack) => (
-                  <Badge variant="outline" key={stack.name}>
-                    {stack.name}
-                  </Badge>
-                ))}
-              </footer>
-            </aside>
-          </article>
+          <Link key={project.title} href={project.deploy_url} target="_blank">
+            <article className="flex flex-col sm:flex-row gap-8 w-full p-4 rounded-lg hover:bg-secondary/30 transition-colors duration-300">
+              <div className="relative sm:min-w-[290px] h-[195px] rounded-lg bg-secondary overflow-hidden">
+                <Image
+                  src={project.image_src}
+                  alt={project.title}
+                  fill
+                  className="object-cover"
+                />
+              </div>
+              <aside className="flex flex-col gap-4">
+                <H3>{project.title}</H3>
+                <p className="text-muted-foreground">{project.description}</p>
+                <div className="flex flex-row gap-2">
+                  {project.stacks.map((stack) => (
+                    <Badge variant="outline" key={stack.name}>
+                      {stack.name}
+                    </Badge>
+                  ))}
+                </div>
+              </aside>
+            </article>
+          </Link>
         ))}
       </section>
 
       <footer className="flex flex-col sm:flex-row items-center justify-between w-full">
         <p className="text-center sm:text-left max-w-sm text-muted-foreground">
-          Dê uma olhada em meu histórico de projetos. Pronto para iniciar algo
-          incrível?
+          Dê uma olhada em meu histórico de projetos.
         </p>
-        <Button variant="link" className="sm:ml-auto">
-          Ver mais trabalhos <ArrowRight className="w-4 h-4 ml-2" />
-        </Button>
+        <Link href="/projects">
+          <Button variant="link" className="sm:ml-auto">
+            Ver mais trabalhos <ArrowRight className="w-4 h-4 ml-2" />
+          </Button>
+        </Link>
       </footer>
     </Section>
   )
