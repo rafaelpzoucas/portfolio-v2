8bd55a053706f7f253847f486759fc18b2f820aa
Rafael Zoucas <rafaelpzoucas@gmail.com>
2023-12-15 18:31:58 -0300

    Update page.tsx

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,15 +4,18 @@ import Link from 'next/link'
 import { Projects } from '../(sections)/projects'
 import { Aside } from '../aside'
 import { projects } from '../data'
+import { ArrowLeft } from 'lucide-react'
 
 export default function ProjectsPage() {
   return (
     <main className="flex flex-col md:flex-row justify-center h-full px-0 sm:px-4">
       <Aside>
-        <nav>
-          <ul className="hidden md:flex flex-col space-y-4 w-fit overflow-hidden">
+        <nav className="">
+          <ul className="flex flex-col space-y-4 w-fit overflow-hidden">
             <Link href="/">
-              <li className={cn('opacity-30 hover:opacity-100')}>Home</li>
+              <li className="flex flex-row gap-2 items-center opacity-30 hover:opacity-100">
+                <ArrowLeft className="w-4 h-4" /> Home
+              </li>
             </Link>
           </ul>
         </nav>
