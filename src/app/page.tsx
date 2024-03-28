96b924663d64e276db92f2b619f828b12419f787
Rafael Zoucas <rafaelpzoucas@gmail.com>
2024-03-28 09:24:18 -0300

    move cta to the bottom

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,9 +14,9 @@ export default function HomePage() {
       {/* <Metrics /> */}
       <Features />
       <CreativeSteps />
-      <CallToAction />
       <Benefits />
       <Projects />
+      <CallToAction />
       {/* <SocialProof /> */}
       <FAQ />
       <Footer />
